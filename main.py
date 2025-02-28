89bcf12ed4ed0069e04ca10644835dd4109e418e
MahakkapanPlangklang <119285823+MahakkapanPlangklang@users.noreply.github.com>
2025-02-28 23:58:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,23 @@ CORS(app)
 def home():
     return jsonify({"message": "Flask API is running on Render"})
 
-
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
         data = request.get_json()
-        features = [float(data[f]) for f in feature_names]
-        features_array = np.array([features]).reshape(1, -1)
 
+        sex_mapping = {"1": "Male", "2": "Female"}  
+        sex_value = sex_mapping.get(str(data["sex"]), "Male")  
+
+        features = [
+            float(data["bill_length_mm"]),
+            float(data["bill_depth_mm"]),
+            float(data["flipper_length_mm"]),
+            float(data["body_mass_g"]),
+            label_encoders["sex"].transform([sex_value])[0]  
+        ]
+
+        features_array = np.array([features]).reshape(1, -1)
         prediction = model.predict(features_array)
         species_predicted = label_encoders["species"].inverse_transform([prediction[0]])[0]
 
@@ -31,7 +40,6 @@ def predict():
     
     except Exception as e:
         return jsonify({"error": str(e)})
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
